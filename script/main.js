ff3bbe98841e9a5112458ef0a5081f5a4b37c14f
Виталий <programmer@hithub.com>
2020-02-01 18:20:18 +0500

    Сделал скрипт замены svg картинок при наведениях

--- script/main.js
+++ script/main.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 var nojs = document.querySelectorAll('.nojs');
 	nojs.forEach( (item) => item.classList.remove('nojs') );
 
@@ -17,3 +15,60 @@ PHbuttonToggle.addEventListener('click', function(){
 	pageHeader.classList.toggle('page-header--opened');
 	menu.classList.toggle('main-nav--opened');
 });
+
+
+//*****************************
+//***Меняющий цвета логотип**** >>
+//*****************************
+var linkSvgLogo = document.querySelector('.page-header__logo'); //Ссылка с лого
+//Теги <source>
+var svgLogo_desctop = document.querySelector('.page-header__logo-image--desctop'),
+	svgLogo_tablet = document.querySelector('.page-header__logo-image--tablet'),
+	svgLogo_mobile = document.querySelector('.page-header__logo-image--mobile');
+
+//Логотипы на которые надо менять
+var ARRAY_LOGOS = {
+	desctop: 'img/logo-pink-white-desktop.svg',
+	tablet: 'img/logo-pink-white-tablet.svg',
+	mobile: 'img/logo-pink-white-mobile.svg',
+};
+var ARRAY_LOGOS_HOVER = {
+	desctop: 'img/logo-pink-white-desktop--hover.svg',
+	tablet: 'img/logo-pink-white-tablet--hover.svg',
+	mobile: 'img/logo-pink-white-mobile--hover.svg',
+};
+var ARRAY_LOGOS_CLICK = {
+	desctop: 'img/logo-pink-white-desktop--click.svg',
+	tablet: 'img/logo-pink-white-tablet--click.svg',
+	mobile: 'img/logo-pink-white-mobile--click.svg',
+};
+
+function changeToLogo(arrayLogos){
+	if(document.documentElement.clientWidth >= 960){
+		svgLogo_desctop.removeAttribute('srcset');
+		svgLogo_desctop.setAttribute('srcset', arrayLogos.desctop);
+	} else if(document.documentElement.clientWidth >= 660){
+		svgLogo_tablet.removeAttribute('srcset');
+		svgLogo_tablet.setAttribute('srcset', arrayLogos.tablet);
+	} else {
+		svgLogo_mobile.removeAttribute('srcset');
+		svgLogo_mobile.setAttribute('srcset', arrayLogos.mobile);
+	}
+}
+
+linkSvgLogo.addEventListener('mouseover', function(){
+	changeToLogo(ARRAY_LOGOS_HOVER);
+});
+linkSvgLogo.addEventListener('mouseout', function(){
+	changeToLogo(ARRAY_LOGOS);
+});
+linkSvgLogo.addEventListener('mousedown', function(){
+	changeToLogo(ARRAY_LOGOS_CLICK);
+});
+linkSvgLogo.addEventListener('mouseup', function(){
+	changeToLogo(ARRAY_LOGOS_HOVER);
+});
+
+//*****************************
+//***Меняющий цвета логотип**** <<
+//*****************************
