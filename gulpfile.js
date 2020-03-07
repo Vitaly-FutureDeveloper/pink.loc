7a176cdc3af92fff4de4e58c555027dd7c263496
Vitaly-Future-Developer <zarvoron@yandex.ru>
2020-03-07 19:58:47 +0500

    Переделаны классы и стили для страницы photo.html, стилизована планшетная версия

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,16 @@ gulp.task("images", function(){
 	.pipe(gulp.dest("./build/img/"))
 })
 
+gulp.task("svg", function(){
+	return gulp.src("./source/img/**/*.{svg}")
+	.pipe(imagemin([
+		//imagemin.mozjpeg({quality: 75, progressive: true}),
+		//imagemin.optipng({optimizationLevel: 3}),
+		//imagemin.svgo()
+	]))
+	.pipe(gulp.dest("./build/img/"))
+})
+
 gulp.task("webp", function(){
 	return gulp.src("./source/img/**/*.{png,jpg}")
 	.pipe(webp({quality: 80}))
